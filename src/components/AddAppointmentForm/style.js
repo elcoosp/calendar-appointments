7640ec8c1a6a15fc3601167162fc1d524ba455c7
Louis Cospain <elcoosp@gmail.com>
2018-09-24 17:49:18 +0200

    style: 💄 Empty styled components for AddAppointmentForm

---
+++ src/components/AddAppointmentForm/style.js
@@ -0,0 +1,20 @@
+import styled from 'styled-components'
+import { Form as FormikForm, Field as FormikField } from 'formik'
+
+export const Field = styled(FormikField)``
+
+export const Form = styled(FormikForm)``
+
+export const ModalWrapper = styled.section``
+
+export const Error = styled.span``
+
+export const Label = styled.label``
+
+export const ModalClose = styled.button``
+
+export const SubmitButton = styled.button``
+
+export const Title = styled.h3``
+
+export const LabelledInput = styled.fieldset``
